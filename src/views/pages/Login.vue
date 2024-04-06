9bd425439d97e9b6c64c2248843180b560753951
danielludan <danielludan@126.com>
2024-04-06 21:49:52 +0800

    修改login.vue

--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -7,14 +7,15 @@
             <CCard class="p-4">
               <CCardBody>
                 <CForm>
-                  <h1>Login</h1>
-                  <p class="text-body-secondary">Sign In to your account</p>
+                  <h1>登录</h1>
+                  <p class="text-body-secondary">请用您的账户登录</p>
                   <CInputGroup class="mb-3">
                     <CInputGroupText>
                       <CIcon icon="cil-user" />
                     </CInputGroupText>
                     <CFormInput
-                      placeholder="Username"
+                      name="username"
+                      placeholder="用户名"
                       autocomplete="username"
                     />
                   </CInputGroup>
@@ -23,18 +24,19 @@
                       <CIcon icon="cil-lock-locked" />
                     </CInputGroupText>
                     <CFormInput
+                      name="password"
                       type="password"
-                      placeholder="Password"
+                      placeholder="密码"
                       autocomplete="current-password"
                     />
                   </CInputGroup>
                   <CRow>
                     <CCol :xs="6">
-                      <CButton color="primary" class="px-4"> Login </CButton>
+                      <CButton color="primary" class="px-4"> 登录 </CButton>
                     </CCol>
                     <CCol :xs="6" class="text-right">
                       <CButton color="link" class="px-0">
-                        Forgot password?
+                        忘记密码?
                       </CButton>
                     </CCol>
                   </CRow>
@@ -44,14 +46,12 @@
             <CCard class="text-white bg-primary py-5" style="width: 44%">
               <CCardBody class="text-center">
                 <div>
-                  <h2>Sign up</h2>
+                  <h2>注册</h2>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
+                    注册之前请先阅读用户需知，请在注册过程中提供必要的用户信息。
                   </p>
                   <CButton color="light" variant="outline" class="mt-3">
-                    Register Now!
+                    现在注册!
                   </CButton>
                 </div>
               </CCardBody>
