a3218390171f222798875c7c847a473bb22882f3
danielludan <danielludan@126.com>
2024-04-14 19:47:12 +0800

    增加dict相关api，页面调用成功

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,26 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:8000/api/';
 
+const API_LOGIN_URL = API_URL + "token"
+const API_DICT_URL = API_URL + "dict/"
+const API_DICT_CAT_URL = API_URL + "dict/cat"
+
+// 获取当前用户
+const getUser = () => {
+  return JSON.parse(localStorage.getItem('user'));
+}
+
+// 获取当前用户的access token
+const getUserAccessToken = () => {
+  const token = getUser()?getUser().access:"";
+  console.log("token:" + token);
+  return token;
+}
+
+const deleteUser = () => {
+  localStorage.removeItem('user');
+}
+
 // 定义拦截器
 const instance = axios.create({
     baseURL: API_URL,
@@ -9,6 +29,12 @@ const instance = axios.create({
 
 instance.interceptors.request.use(
     config => {
+
+      config.headers = {
+        ...config.headers, // Merge existing headers
+        'Authorization': 'Bearer ' + getUserAccessToken(), // Add your custom header
+      };
+
         // 可以在这里添加请求头或其他配置
         return config;
     },
@@ -50,8 +76,10 @@ instance.interceptors.response.use(
     });
 
 class API {
+
+  // 登录
   login(username, password) {
-    return instance.post(API_URL + 'token', {
+    return instance.post(API_LOGIN_URL, {
       username,
       password,
     }).then(response => {
@@ -62,13 +90,47 @@ class API {
     });
   }
 
+  // 注销
   logout() {
-    localStorage.removeItem('user');
+    deleteUser();
   }
 
-  getUser() {
-    return JSON.parse(localStorage.getItem('user'));
+  // Get dictionary list
+  dict_list(params = {}) {
+    return instance.get(API_DICT_URL, {
+      params, // Pass query parameters for filtering
+    }).then((response) => {
+      const { results, count } = response.data; // Extract data from response
+      return { results, count }; // Return data and total count
+    });
   }
+
+  // Get dictionary details
+  dict_detail(id) {
+    return instance.get(API_DICT_URL + `${id}/`).then((response) => {
+      return response.data; // Return dictionary details
+    });
+  }
+
+  // Create a new dictionary
+  dict_create(data) {
+    return instance.post(API_DICT_URL, data).then((response) => {
+      return response.data; // Return created dictionary
+    });
+  }
+
+  // Update an existing dictionary
+  dict_update(id, data) {
+    return instance.put(API_DICT_URL + `${id}/`, data).then((response) => {
+      return response.data; // Return updated dictionary
+    });
+  }
+
+  // Delete a dictionary
+  dict_delete(id) {
+    return instance.delete(API_DICT_URL + `${id}/`); // Delete dictionary
+  }
+
 }
 
 export default new API();
--- src/views/dict/list.vue
+++ src/views/dict/list.vue
@@ -6,34 +6,44 @@
             <strong>列表</strong>
           </CCardHeader>
           <CCardBody>
-            <EasyDataTable
+            <!-- <EasyDataTable
                 :headers="headers"
                 :items="items"
-            />
+            /> -->
           </CCardBody>
         </CCard>
       </CCol>
     </CRow>
   </template>
   
-  <script lang="ts" setup>
-  import type { Header, Item } from "vue3-easy-data-table";
+  <script setup>
+//   import type { Header, Item } from "vue3-easy-data-table";
+  import API from "@/services/api"
   
-  const headers: Header[] = [
-    { text: "PLAYER", value: "player" },
-    { text: "TEAM", value: "team"},
-    { text: "NUMBER", value: "number"},
-    { text: "POSITION", value: "position"},
-    { text: "HEIGHT", value: "indicator.height"},
-    { text: "WEIGHT (lbs)", value: "indicator.weight", sortable: true},
-    { text: "LAST ATTENDED", value: "lastAttended", width: 200},
-    { text: "COUNTRY", value: "country"},
-  ];
+  const list = async () => {
+    const { response, count } = await API.dict_list();
+    console.log(response);
+    console.log(count);
+  };
   
-  const items: Item[] = [
-    { player: "Stephen Curry", team: "GSW", number: 30, position: 'G', indicator: {"height": '6-2', "weight": 185}, lastAttended: "Davidson", country: "USA"},
-    { player: "Lebron James", team: "LAL", number: 6, position: 'F', indicator: {"height": '6-9', "weight": 250}, lastAttended: "St. Vincent-St. Mary HS (OH)", country: "USA"},
-    { player: "Kevin Durant", team: "BKN", number: 7, position: 'F', indicator: {"height": '6-10', "weight": 240}, lastAttended: "Texas-Austin", country: "USA"},
-    { player: "Giannis Antetokounmpo", team: "MIL", number: 34, position: 'F', indicator: {"height": '6-11', "weight": 242}, lastAttended: "Filathlitikos", country: "Greece"},
-  ];
+  list();
+  
+
+//   const headers: Header[] = [
+//     { text: "PLAYER", value: "player" },
+//     { text: "TEAM", value: "team"},
+//     { text: "NUMBER", value: "number"},
+//     { text: "POSITION", value: "position"},
+//     { text: "HEIGHT", value: "indicator.height"},
+//     { text: "WEIGHT (lbs)", value: "indicator.weight", sortable: true},
+//     { text: "LAST ATTENDED", value: "lastAttended", width: 200},
+//     { text: "COUNTRY", value: "country"},
+//   ];
+  
+//   const items: Item[] = [
+//     { player: "Stephen Curry", team: "GSW", number: 30, position: 'G', indicator: {"height": '6-2', "weight": 185}, lastAttended: "Davidson", country: "USA"},
+//     { player: "Lebron James", team: "LAL", number: 6, position: 'F', indicator: {"height": '6-9', "weight": 250}, lastAttended: "St. Vincent-St. Mary HS (OH)", country: "USA"},
+//     { player: "Kevin Durant", team: "BKN", number: 7, position: 'F', indicator: {"height": '6-10', "weight": 240}, lastAttended: "Texas-Austin", country: "USA"},
+//     { player: "Giannis Antetokounmpo", team: "MIL", number: 34, position: 'F', indicator: {"height": '6-11', "weight": 242}, lastAttended: "Filathlitikos", country: "Greece"},
+//   ];
   </script>
